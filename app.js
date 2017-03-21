3aa299ae19a28257b3aa4122291b0c7b1af21fc2
Pasha-Marki <PashaRMarki@gmail.com>
2017-03-20 21:57:02 -0700

    switched folders

--- app.js
+++ app.js
@@ -4,20 +4,38 @@ var io = require('socket.io')(http);
 var path = require('path');
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  var use = $_GET["user"];
+  if(use!=""){
+
+  }
+  res.sendFile(path.join(__dirname,'/index.html'));
 });
 users = [];
+
 io.on('connection', function(socket){
   console.log('A user connected');
   socket.on('setUsername', function(data){
-    console.log(data);
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function(){
+      socket.emit('Login', 'Hash is: ' + this.responseText)
+    };
+    xmlhttp.open("POST", "login.php", true);
+    xmlhttp.send("info = "+myObj);
+
+
+
+
+
+
+
+    /*console.log(data);
     if(users.indexOf(data) > -1){
       socket.emit('userExists', data + ' username is taken! Try some other username.');
     }
     else{
       users.push(data);
       socket.emit('userSet', {username: data});
-    }
+    }*/
   });
   socket.on('msg', function(data){
       //Send message to everyone
@@ -27,4 +45,3 @@ io.on('connection', function(socket){
 http.listen(8888, function(){
   console.log('listening on localhost:8888');
 });
-
