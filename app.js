f705afd4bc139f73c4b97646ed0136a6655df948
Pasha <PashaRMarki@gmail.com>
2018-02-27 03:37:33 +0000

    Working es6 chatroom

--- app.js
+++ app.js
@@ -1,102 +1,153 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var path = require('path');
-var req = require('request');
-var mysql = require('mysql');
-var bcrypt = require('bcrypt');
-var xss = require('xss');
-var options = {
-  whiteList: {
-    a: ['href', 'title', 'target'],
-    b: true,
-    i: true,
-    u: true,
-    s: true
+import express from 'express';
+import HTTP from 'http';
+var socketio = require('socket.io');
+import "babel-polyfill";
+import req from 'request';
+import bcrypt from 'bcrypt';
+import xss from 'xss';
+import assert from 'assert';
+import Mongo from 'mongodb';
 
-  }
-};
-var myxss = new xss.FilterXSS(options);
-
-
+const path = require('path');
 const saltRounds = 10;
-var pool = mysql.createPool({
-  connectionLimit : 100,
-  host    : '',
-  port    : '',
-  user    : '',
-  password: '',
-  database: '',
-  debug   : false
-});
+const url = 'mongodb://localhost:27017';
+const dbName = 'chatbox';
+const options = {
+	whiteList: {
+		a: ['href', 'title', 'target'],
+		b: true,
+		i: true,
+		u: true,
+		s: true
+	}
+};
+const port = 8080;
 
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname,'/index.html'));
-});
+const app = express();
+const http = HTTP.createServer(app);
+const io = socketio(http, {path: '/chatbox', serveClient: false});
+const MongoClient = Mongo.MongoClient;
+const myxss = new xss.FilterXSS(options);
 
+const InsertUser = async function(username, passwordHash){
+	let client;
+	try {
+		client = await MongoClient.connect(url);
+		console.log("Connected to server");
 
+		const db = client.db(dbName);
+		const collection = db.collection('users');
 
-io.on('connection', function(socket){
-  console.log('A user connected');
-  socket.on('getUser', function(data){
-    bcrypt.hash(data.password, saltRounds, function(err, hash){
-      if(err){
-        console.error(err);
-      }
-      pool.getConnection(function(err, connection){
-        if(err) {
-          console.error("error connecting to db" + err);
-          return;
-        }
+		let is_user = await collection.findOne({
+			name: username
+		});
 
-        console.log('connected as id ' + connection.threadId);
+		if(is_user){
+			client.close();
+			return 'repeatUser';
+		} else {
+			assert.equal(null, is_user);
 
-        connection.query("select username, password from users where username = '??'", [data.username], function(error, results, fields){
-          bcrypt.compare(data.password, results[0][1],function(err, res) {
-            console.log(res);
-            socket.emit('userSet', data);
-          });
+			let insert_user = await db.collection('users').insertOne({
+				  name: username,
+				  password: passwordHash
+			  }, {
+				  w: 'majority',
+				  wtimeout: 1000,
+				  forceServerObjectId: true
+			  }
+		  );
 
-        });
-      });
+			assert.equal(1, insert_user.insertedCount);
 
-    });
-  });
+			client.close();
+			return 'userSet';
+		}
+	} catch (err) {
+		console.log(err);
+	}
 
-  socket.on('signUp', function(data){
-    bcrypt.genSalt(saltRounds, function(err, salt){
-      if(err){console.error('salt error', err);}
-      bcrypt.hash(data.password, salt, function(err, hash){
-        if(err){console.error('hash error', err);}
-        pool.getConnection(function(err, connection){
-          if(err){console.error('connection error', err);}
-          connection.query("select username from users where username = '??'", [data.username], function(error, results, fields){
-            if(error){console.error('initial query error', error);}
-            console.log("got here");
-            if(results[0]==null){
-              connection.query("insert into users (username, password, salt) values ('??','??', '??')", [data.username, hash, salt], function(error, results, fields){
-                if(error){console.error('query error', error);}
-                else{
-                socket.emit('userSet', {username: data.username});
-                console.log("user signed in");
-              }
-              });
-            }else socket.emit('repeatUser', {username: data.username});
-          });
-        });
-      });
-    });
-  });
+	client.close();
+};
 
-  socket.on('msg', function(data){
-      //Send message to everyone
-      io.sockets.emit('newmsg', {user: myxss.process(data.user), message: myxss.process(data.message)});
-  });
-});
+const GetUserPassword = async function(username, passwordHash){
+	let client;
+	try {
+		client = await MongoClient.connect(url);
+		console.log("Connected to server");
 
-function escapeScript(str){
-  return str.replace(/<script>|script>/ig,"");
+		const db = client.db(dbName);
+
+		let get_user = await db.collection('users').findOne({
+			name: username
+		});
+		if(get_user){
+		return get_user.password; 
+		}
+		else{
+		throw 'incorrect user';
+		}
+	} catch(err) {
+		throw err;
+	};
 }
-http.listen(8888, function(){
-  console.log('listening on localhost:8888');
+
+app.get('/chatbox', (req, res) => {
+	res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.get('/chatbox/resources/socketjs', function(req, res) {
+  console.log('send js file');
+  res.sendFile(path.join(__dirname, '/node_modules/socket.io-client/dist/socket.io.js'));
+});
+
+
+io.use((socket, next) => {
+  console.log('socket connection');
+  return next();
+});
+
+io.on('connection', (socket) => {
+	console.log('A user connected');
+
+	socket.on('getUser', (data) => {
+		GetUserPassword(data.username).then((password) => (
+			bcrypt.compare(data.password, password)
+		)).then((res) => {
+			if(res){
+				socket.emit('userSet', {username: data.username})
+			}
+			else {
+				throw('incorrect password')};
+		}).catch((err) => {
+			if(err == 'incorrect user' || err == 'incorrect password'){
+				console.log(err);
+				socket.emit('badUser', {username: data.username});
+			}
+			else{
+				console.log(err);
+			}
+		});
+	});
+
+	socket.on('signUp', function(data){
+		bcrypt.hash(data.password, saltRounds).then((hash) => (
+			InsertUser(data.username, hash)
+		)).then((result) => {
+			socket.emit(result, {username: data.username});
+		});
+	});
+
+	socket.on('msg', (data) => {
+		io.sockets.emit('newmsg', {user: myxss.process(data.user), message: myxss.process(data.message)});
+	});
+
+	socket.on('disconnect', function(){
+		console.log('disconnected');
+	});
+});
+
+
+http.listen(port, () => {
+  console.log('listening on port ' + port);
 });
